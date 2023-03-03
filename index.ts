fd344b7f4f55e00cc0af2d7d90ef64b91514c5ec
veronicaisabelpereira <veronicaisabelpereira80@gmail.com>
2023-03-03 14:20:07 -0300

    environment vars

--- index.ts
+++ index.ts
@@ -3,9 +3,17 @@ import * as express from "express";
 const app = express();
 const PORT = 3000;
 
+console.log("Mi nombre es ", process.env.NAME);
+
+
 app.get("/hola", (req, res) => {
   res.json({
     message: "Hola mundo, desde el servidor",
+  });
+});
+app.get("/env", (req, res) => {
+  res.json({
+    environment: process.env.ENVIRONMENT,
   });
 });
 app.listen(PORT, () => {
